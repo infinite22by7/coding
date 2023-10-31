1b3fb0f0ea989e27616b3e4382ae98732235fff5
Akash Kumar <akashkumar@Akashs-MacBook-Air.local>
2023-10-31 10:11:40 +0530

    infinite

---
+++ c++/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // fuck off
+    /*
+
+    */
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    int roll = 13;
+    char sec = 'D';
+    string name = "fuck off";
+    bool pass = false;
+    double percentile = 12.9999999;
+    float percent = 13;
+
+    cout << roll;
+    cout << "size of string " << sizeof(string) << endl;
+    cout << sec << sizeof(sec) << endl;
+    cout << pass << " " << sizeof(pass) << endl;
+    cout << percentile<< sizeof(percentile)<<endl;
+}
---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+//variable  assigment
+//let, const,var variablename= value
+let name = "akash";
+console.log(name)
+let roll = 13
+console.log(roll)
